07667ce4b3e2739647f2f8517bc654304d235c5d
vrnblr <spamvrnblr@gmail.com>
2020-04-23 21:47:59 +0300

    Updates

---
+++ precache-manifest.ded50c0d4d8c705c4cf117d72eedae8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f54e1def85adac3a134277d393bc205",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f7635257ff782763ec8f",
+    "url": "/tic-tac-toe/static/css/main.84226012.chunk.css"
+  },
+  {
+    "revision": "8e815d68caae67b7da1b",
+    "url": "/tic-tac-toe/static/js/2.b849af13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.b849af13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7635257ff782763ec8f",
+    "url": "/tic-tac-toe/static/js/main.515151b3.chunk.js"
+  },
+  {
+    "revision": "28dfba9420967d301740",
+    "url": "/tic-tac-toe/static/js/runtime-main.4c12a8d1.js"
+  }
+]);
